078e6f1f75790f34c2ab9f5fce86cb33e8e57071
Jesús González <memije.dev@gmail.com>
2020-11-13 14:40:37 -0600

    Cambios en el menú

--- resources/views/admin/layouts/partials/menu.blade.php
+++ resources/views/admin/layouts/partials/menu.blade.php
@@ -18,24 +18,13 @@
         <span>Dashboard</span></a>
     </li>
 
-
-
     <!-- Divider -->
     <hr class="sidebar-divider">
 
     <!-- Heading -->
     <div class="sidebar-heading">
-      Sistema
+      Core
     </div>
-
-    <!-- Nav Item - Users -->
-    <li class="nav-item">
-      <a class="nav-link" href="{{ route('users.index') }}">
-        <i class="fas fa-fw fa-user"></i>
-        <span>Usuarios</span></a>
-    </li>
-
-
 
      <li class="nav-item">
       <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseProject" aria-expanded="true" aria-controls="collapseProject">
@@ -65,8 +54,29 @@
       </div>
     </li>
 
-  
+    <!-- Divider -->
+    <hr class="sidebar-divider">
 
+    <!-- Heading -->
+    <div class="sidebar-heading">
+      Sistema
+    </div>
+
+    <!-- Nav Item - Users -->
+    <li class="nav-item">
+      <a class="nav-link" href="{{ route('users.index') }}">
+        <i class="fas fa-fw fa-user"></i>
+        <span>Usuarios</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Heading -->
+    <div class="sidebar-heading">
+      Extras
+    </div>
+    
     <!-- Nav Item - Pages Collapse Menu -->
     <li class="nav-item">
       <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
@@ -76,27 +86,27 @@
       <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
         <div class="bg-white py-2 collapse-inner rounded">
           <h6 class="collapse-header">Login Screens:</h6>
-          <a class="collapse-item" href="login.html">Login</a>
-          <a class="collapse-item" href="register.html">Register</a>
-          <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
+          <a class="collapse-item" href="#">Login</a>
+          <a class="collapse-item" href="#">Register</a>
+          <a class="collapse-item" href="#">Forgot Password</a>
           <div class="collapse-divider"></div>
           <h6 class="collapse-header">Other Pages:</h6>
-          <a class="collapse-item" href="404.html">404 Page</a>
-          <a class="collapse-item" href="blank.html">Blank Page</a>
+          <a class="collapse-item" href="#">404 Page</a>
+          <a class="collapse-item" href="#">Blank Page</a>
         </div>
       </div>
     </li>
 
     <!-- Nav Item - Charts -->
     <li class="nav-item">
-      <a class="nav-link" href="charts.html">
+      <a class="nav-link" href="#">
         <i class="fas fa-fw fa-chart-area"></i>
         <span>Charts</span></a>
     </li>
 
     <!-- Nav Item - Tables -->
     <li class="nav-item">
-      <a class="nav-link" href="tables.html">
+      <a class="nav-link" href="#">
         <i class="fas fa-fw fa-table"></i>
         <span>Tables</span></a>
     </li>
