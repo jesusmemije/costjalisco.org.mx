17deaf7c116e8e73c84f11bb31054eba3e9bc48a
Jesús González <memije.dev@gmail.com>
2020-11-20 09:28:35 -0600

    Seed News

--- database/migrations/2020_11_04_100730_create_news_table.php
+++ database/migrations/2020_11_04_100730_create_news_table.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class CreateNewsTable extends Migration
 {
@@ -24,6 +26,29 @@ class CreateNewsTable extends Migration
             $table->charset = 'utf8mb4';
             $table->collation = 'utf8mb4_spanish_ci';
         });
+
+        // Seed table news
+        $news = [];
+
+        $news[] = ["id" => "1", "title" => "PICE Software Inc.", "content" => "<h2><strong>Basic Card Example</strong></h2>
+        <p><strong><a href='http://pice-software.com'>PICE Software</a></strong></p>", "author" => "Jesús González Memije", "published" => "0"];
+        $news[] = ["id" => "2", "title" => "Cost Jalisco, GDL.", "content" => "", "author" => "", "published" => "0"];
+
+        $date = Carbon::now();
+
+        // Create rows
+        foreach($news as $new)
+        {
+            DB::table('news')->insert([
+                'id'         => $new['id'],
+                'title'      => $new['title'],
+                'content'    => $new['content'],
+                'author'     => $new['author'],
+                'published'  => $new['published'],
+                'created_at' => $date,
+                'updated_at' => $date,
+            ]);
+        }
     }
 
     /**
