78ac8cb56f5fb907f1628dfd6e755a4c2ac5f1ab
Jesús González <memije.dev@gmail.com>
2020-11-24 10:33:16 -0600

    Footer

--- resources/views/front/layouts/partials/footer.blade.php
+++ resources/views/front/layouts/partials/footer.blade.php
@@ -1,3 +1,93 @@
-<footer>
-
+<!-- Footer -->
+<footer class="page-footer font-small" style="background-color: #628EA0">
+    <div style="background-color: #58707B;">
+        <div class="container">
+            <!-- Grid row-->
+            <div class="row d-flex align-items-center">
+                <!-- Grid column -->
+                <div class="col-md-6 col-lg-7 text-center text-md-center" style="letter-spacing: 1px;">
+                    <h5 class="pt-4 text-white">ENLACES DE INTERÉS</h5>
+                </div>
+                <!-- Grid column -->
+                <div class="col-md-6 col-lg-5 text-center text-md-center" style="letter-spacing: 1px;">
+                    <h5 class="pt-4 text-white">CONTACTO</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Footer Links -->
+    <div class="container text-center text-md-left mt-5">
+        <!-- Grid row -->
+        <div class="row mt-3">
+            <!-- Grid column -->
+            <div class="col-md-7 col-lg-7 mx-auto mb-4">
+                <!-- Content -->
+                <div class="row mb-4">
+                    <div class="col-md-5">
+                        <a href="#" target="_BLANK">
+                            <img src="{{ asset('assets/img/footer/cost.png') }}" class="img-fluid" alt="">
+                        </a>
+                    </div>
+                    <div class="col-md-4">
+                        <a href="#" target="_BLANK">
+                            <img src="{{ asset('assets/img/footer/itei.png') }}" class="img-fluid" alt="">
+                        </a>
+                    </div>
+                    <div class="col-md-3">
+                        <a href="#" target="_BLANK">
+                            <img src="{{ asset('assets/img/footer/inspect-cost.png') }}" class="img-fluid" alt="">
+                        </a>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-3">
+                        <a href="#" target="_BLANK">
+                            <img src="{{ asset('assets/img/footer/zapopan.png') }}" class="img-fluid" alt="">
+                        </a>
+                    </div>
+                    <div class="col-md-3">
+                        <a href="#" target="_BLANK">
+                            <img src="{{ asset('assets/img/footer/guadalajara.png') }}" class="img-fluid" alt="">
+                        </a>
+                    </div>
+                    <div class="col-md-3">
+                        <a href="#" target="_BLANK">
+                            <img src="{{ asset('assets/img/footer/jalisco.png') }}" class="img-fluid" alt="">
+                        </a>
+                    </div>
+                    <div class="col-md-3">
+                        <a href="#" target="_BLANK">
+                            <img src="{{ asset('assets/img/footer/tonala.png') }}" class="img-fluid" alt="">
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <!-- Grid column -->
+            <div class="col-md-4 col-lg-4 mx-auto mb-md-0 mb-4 text-white">
+                <!-- Links -->
+                <p><i class="fas fa-mobile-alt mr-3 fa-lg"></i> <a href="#">33 3630-5745 ext 1510</a></p>
+                <p><i class="fas fa-envelope mr-3"></i> <a href="#">jalisco@infrastructuretransparency.org</a></p>
+                <p><i class="fas fa-globe mr-3"></i> <a href="#">https://infrastructuretransparency.org</a></p>
+        
+                <p style="margin-left: 34px"> <a href="">REVISA EL AVISO DE PRIVACIDAD</a></p>
+            </div>
+            <!-- Grid column -->
+            <div class="col-md-1 col-lg-1 text-center mx-auto my-4">
+                <div class="social">
+                    <ul>
+                        <li><a href="#" target="_blank" class="icon-facebook"><i
+                                    class="fab fa-facebook-f fa-sm"></i></a></li>
+                        <li><a href="#" target="_blank" class="icon-youtube"><i class="fab fa-youtube fa-sm"></i></a>
+                        </li>
+                        <li><a href="#" target="_blank" class="icon-twitter"><i class="fab fa-twitter fa-sm"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Copyright -->
+    <div class="footer-copyright text-center text-white py-3" style="background-color: #2C4143; font-size: 14px;">
+        <strong>CoST Jalisco 2020</strong> | Todos los derechos reservados
+    </div>
 </footer>
