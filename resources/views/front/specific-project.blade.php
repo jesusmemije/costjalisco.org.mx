ef558f79a4c42d3cbef2bda479cc652b95da60d1
estelamarsan19 <estelamar1911@gmail.com>
2020-12-15 15:28:35 -0600

    cambio del slider

--- resources/views/front/specific-project.blade.php
+++ resources/views/front/specific-project.blade.php
@@ -11,59 +11,13 @@ Revestimiento y saneamiento del canal de aguas pluviales
 
 @section('content')
 
-
-            <!--<img src="{{ asset('assets/img/project/proyecto-2.jpg') }}" class="img-fluid" alt="">-->
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Bootstrap Example</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-
-  <div class="container-fluid pt-4">
+<div class="container-fluid pt-4">
     <!-- Section - Descripción General del proyecto -->
-    <div id="myCarousel" class="carousel slide" data-ride="carousel">
     <div class="row" style="background-color: #d8d8cd;">
         <div class="col-md-3 px-0">
-       
-    <!-- Indicators -->
-    <ol class="carousel-indicators">
-      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-      <li data-target="#myCarousel" data-slide-to="1"></li>
-      <li data-target="#myCarousel" data-slide-to="2"></li>
-    </ol>
-
-    <!-- Wrapper for slides -->
-    <div class="carousel-inner">
-      <div class="item active">
-       
-      <img src="{{ asset('assets/img/project/proyecto-2.jpg') }}" class="img-fluid" alt="">
-      </div>
-
-      <div class="item">
-      <img src="{{ asset('assets/img/project/proyecto-2.jpg') }}" class="img-fluid" alt="">
-      </div>
-    
-      <div class="item">
-      <img src="{{ asset('assets/img/project/proyecto-2.jpg') }}" class="img-fluid" alt="">
-      </div>
-    </div>
-
-    <!-- Left and right controls -->
-    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-      <span class="glyphicon glyphicon-chevron-left"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#myCarousel" data-slide="next">
-      <span class="glyphicon glyphicon-chevron-right"></span>
-      <span class="sr-only">Next</span>
-    </a>
-  </div>
-
-  <div class="col-md-9 px-0">
+            <img src="{{ asset('assets/img/project/proyecto-2.jpg') }}" class="img-fluid" alt="">
+        </div>
+        <div class="col-md-9 px-0">
             <div class="media-body">
                 <div id="titleproject" class="col-md-12">
                     <span>REVESTIMIENTO Y SANEAMIENTO DEL <br> CANAL DE AGUAS PLUVIALES</span>
@@ -120,15 +74,6 @@ Revestimiento y saneamiento del canal de aguas pluviales
         </div>
     </div>
 
-</div>
-
-
-</body>
-</html>
-   
-
-
-    
     <!-- Section - Mapa de la localización -->
     <div class="row" id="map"></div>
 
