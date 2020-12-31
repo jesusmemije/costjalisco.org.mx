91ff380d9ee9893f866547f5e98caffaa9056b6d
estelamarsan19 <estelamar1911@gmail.com>
2020-12-30 18:51:34 -0600

    inspect

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -9,13 +9,49 @@ Inicio
 <!-- Contenido -->
 <div class="main">
 
-    <div class="chatbot">
-        <a href="">
-            <img src="{{ asset('assets/img/home/chatbot.png') }}" class="img-fluid" width="280"
-                alt="Chatbot - Página CoST Jalisco">
-        </a>
-    </div>
+<!--MODAL-->
+<div class="chatbot text-center">
+  
 
+    <img src="{{ asset('assets/img/home/chatbot.png') }}" type="button" 
+    class="img-fluid" width="280" alt="Chatbot - Página CoST Jalisco"
+    data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">
+</div>
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Titulo</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+        </button>
+        
+      </div>
+      <div class="modal-body">
+        <form>
+          
+          <div class="mb-3">
+            <label for="message-text" class="col-form-label">Mensaje:</label>
+            <textarea class="form-control" id="message-text"></textarea>
+          </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+        <button type="button" class="btn btn-primary">Enviar</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+
+
+
+
+    
     <!-- Section - Carousel main -->
     <div id="carouselHome" class="carousel slide" data-ride="carousel">
         <ol class="carousel-indicators">
@@ -98,6 +134,7 @@ Inicio
         </div>
     </div>
 -->
+
     <!-- Título - Nosotros -->
     <div class="container">
         <div class="row">
