7d9ca328a4a50f763352163bf7968a4811248a03
Jesús González <memije.dev@gmail.com>
2021-01-14 13:13:17 -0600

    About us and knoew more responsive

--- resources/views/front/about-us.blade.php
+++ resources/views/front/about-us.blade.php
@@ -6,14 +6,23 @@ Nosotros
 
 @section('content')
 
+<style>
+
+@media only screen and (max-width: 480px) {
+    .title-barra-roja {
+        font-size: 24px;
+    }
+}
+    
+</style>
+
 <div class="main">
 
     <!-- Title - CoST Jalisco -->
     <div class="row mx-0 my-4" id="cost-jalisco">
         <div class="col-md-6 px-0">
             <div class="text-center text-white">
-                <h3 class="py-2 font-weight-bold" style='background-image: url("http://pice-software.com/costjalisco/public/assets/img/background-rojo.jpg"); background-repeat: no-repeat;
-                background-size: cover;'>CoST Jalisco</h3>
+                <h3 class="py-2 font-weight-bold title-barra-roja">CoST Jalisco</h3>
             </div>
         </div>
     </div>
@@ -23,44 +32,44 @@ Nosotros
         <div class="row">
             <div class="col-md-12">
                 <p>
-                    En América Latina, los gobiernos son frecuentemente cuestionados por la falta de transparencia en la gestión pública; lo <br>
+                    En América Latina, los gobiernos son frecuentemente cuestionados por la falta de transparencia en la gestión pública; lo <br class="hidden-phone">
                     cual, afecta directamente su credibilidad ante los ciudadanos.
                 </p>
                 <p>
-                    En México, la corrupción se ha vuelto una constante, invadiendo diferentes sectores de la administración pública dentro de <br>
+                    En México, la corrupción se ha vuelto una constante, invadiendo diferentes sectores de la administración pública dentro de <br class="hidden-phone">
                     todos los niveles de Gobierno.
                 </p>
                 <p>
-                    Uno de los sectores con más incidencia de actos de corrupción, es la industria de la construcción a partir de la ejecución de <br>
-                    obra pública, donde sus consecuencias se reflejan en: presupuestos sobregirados y costos elevados, uso de materiales de <br>
-                    baja calidad, omisión de estudios y proyectos, baja rentabilidad económica y social, opacidad en las contrataciones, entre <br>
+                    Uno de los sectores con más incidencia de actos de corrupción, es la industria de la construcción a partir de la ejecución de <br class="hidden-phone">
+                    obra pública, donde sus consecuencias se reflejan en: presupuestos sobregirados y costos elevados, uso de materiales de <br class="hidden-phone">
+                    baja calidad, omisión de estudios y proyectos, baja rentabilidad económica y social, opacidad en las contrataciones, entre <br class="hidden-phone">
                     otras.
                 </p>
                 <p>
-                    Ante este escenario y, a sabiendas de que la ejecución de obra pública es una pieza fundamental en el desarrollo y el <br>
-                    crecimiento económico de un estado o país, fue que el Instituto de Transparencia, Información Pública y Protección de Datos <br>
+                    Ante este escenario y, a sabiendas de que la ejecución de obra pública es una pieza fundamental en el desarrollo y el <br class="hidden-phone">
+                    crecimiento económico de un estado o país, fue que el Instituto de Transparencia, Información Pública y Protección de Datos <br class="hidden-phone">
                     Personales del Estado de Jalisco (ITEI) decidió aplicar a la iniciativa internacional de Transparencia en Infraestructura CoST.
                 </p>
                 <p>
-                    Derivado de lo anterior, el Instituto de Transparencia, Información, Pública y Protección de Datos Personales del Estado de <br>
-                    Jalisco (ITEI) logró captar el interés del Gobierno del Estado de Jalisco y de los gobiernos municipales de Guadalajara, <br>
-                    Tlajumulco de Zuñiga, Tonalá y Zapopan para combatir las prácticas de corrupción dentro de sus proyectos de <br>
-                    Infraestructura; por lo cual, aplicó en septiembre de 2019 para adherirse como miembro de la iniciativa internacional "CoST", <br>
+                    Derivado de lo anterior, el Instituto de Transparencia, Información, Pública y Protección de Datos Personales del Estado de <br class="hidden-phone">
+                    Jalisco (ITEI) logró captar el interés del Gobierno del Estado de Jalisco y de los gobiernos municipales de Guadalajara, <br class="hidden-phone">
+                    Tlajumulco de Zuñiga, Tonalá y Zapopan para combatir las prácticas de corrupción dentro de sus proyectos de <br class="hidden-phone">
+                    Infraestructura; por lo cual, aplicó en septiembre de 2019 para adherirse como miembro de la iniciativa internacional "CoST", <br class="hidden-phone">
                     con el apoyo de instituciones del sector público y privado, así como de organizaciones de la sociedad civil.
                 </p>
                 <p>
-                    Como resultado del proceso de aplicación, el 18 de octubre de 2019, mediante una carta de aprobación expedida por la <br>
-                    Junta de CoST Internacional, el ITEI fue notificado sobre la aprobación para que, por su conducto, el estado de Jalisco, forme <br>
+                    Como resultado del proceso de aplicación, el 18 de octubre de 2019, mediante una carta de aprobación expedida por la <br class="hidden-phone">
+                    Junta de CoST Internacional, el ITEI fue notificado sobre la aprobación para que, por su conducto, el estado de Jalisco, forme <br class="hidden-phone">
                     parte de esta iniciativa internacional.
                 </p>
                 <p>
-                    Con su implementación, pretende mejorar la transparencia en los procesos de infraestructura, exigiendo la publicación de <br>
-                    información que contenga datos claves de todo el ciclo del proyecto (identificación, preparación, contratación, ejecución y <br>
-                    evaluación) a través de herramientas tecnológicas basadas en plataformas de información focalizadas que servirán como <br>
+                    Con su implementación, pretende mejorar la transparencia en los procesos de infraestructura, exigiendo la publicación de <br class="hidden-phone">
+                    información que contenga datos claves de todo el ciclo del proyecto (identificación, preparación, contratación, ejecución y <br class="hidden-phone">
+                    evaluación) a través de herramientas tecnológicas basadas en plataformas de información focalizadas que servirán como <br class="hidden-phone">
                     medio de divulgación.
                 </p>
                 <p>
-                    La iniciativa de CoST en Jalisco está liderada por el Grupo Multisectorial "GMS", quien es el responsable de guiar el <br>
+                    La iniciativa de CoST en Jalisco está liderada por el Grupo Multisectorial "GMS", quien es el responsable de guiar el <br class="hidden-phone">
                     desarrollo, la implementación y supervisión de la iniciativa en el estado.
                 </p>
             </div>
@@ -71,20 +80,19 @@ Nosotros
     <div class="row mx-0 my-4" id="objetivo-general">
         <div class="col-md-6 px-0">
             <div class="text-center text-white">
-                <h3 class="py-2 font-weight-bold" style='background-image: url("http://pice-software.com/costjalisco/public/assets/img/background-rojo.jpg"); background-repeat: no-repeat;
-                background-size: cover;'>Objetivo General</h3>
+                <h3 class="py-2 font-weight-bold title-barra-roja">Objetivo General</h3>
             </div>
         </div>
     </div>
 
     <!-- Section - Objetivo General -->
-    <div class="container mb-5">
+    <div class="container mb-md-5">
         <div class="row">
             <div class="col-md-12">
                 <p>
-                    Fortalecer la transparencia y la rendición de cuentas en las inversiones de infraestructura pública en el Estado de Jalisco <br>
-                    mediante la publicación de información de los procesos de obra pública de forma proactiva y en datos abiertos, a través de <br>
-                    una plataforma tecnológica, que permita mejorar la eficiencia de los recursos ejercidos por las entidades públicas <br>
+                    Fortalecer la transparencia y la rendición de cuentas en las inversiones de infraestructura pública en el Estado de Jalisco <br class="hidden-phone">
+                    mediante la publicación de información de los procesos de obra pública de forma proactiva y en datos abiertos, a través de <br class="hidden-phone">
+                    una plataforma tecnológica, que permita mejorar la eficiencia de los recursos ejercidos por las entidades públicas <br class="hidden-phone">
                     participantes dentro de la iniciativa CoST en Jalisco, impactando directamente en favor del combate a la corrupción.
                 </p>
             </div>
