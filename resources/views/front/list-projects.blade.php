057cce7a0045be3962f63a137dd0cfbbd81de25f
Pablo98d <60664025+Pablo98d@users.noreply.github.com>
2021-01-28 22:39:01 -0600

    [UPDATE]
    list-project

--- resources/views/front/list-projects.blade.php
+++ resources/views/front/list-projects.blade.php
@@ -10,38 +10,14 @@ Listado de obras
 
 @section('content')
 
-<div class="container-fluid pt-4 container-card">
-    <!-- Section - Datos generales -->
-    <div class="row mt-3" id="datos-generales" style="background: #fff">
+<div class="container-fluid py-4">
+
+    <div class="row" style="background: #fff">
         <div class="col-lg-6 col-md-12 col-sm-12 background-title bg-rojo px-0 py-1">
             <span class="topic">Listado de Obras</span>
         </div>
     </div>
-    <div class="container mx-container">
-        <div class="container-subtopic" id="sectorpublico">
-            <h3 class="subtopic">Sector Público</h3>
-        </div>
 
-        @foreach ($projects as $project)
-
-        <div class="media">
-            @php
-            $imagen = DB::table('projects_imgs')
-            ->select('projects_imgs.imgroute')
-            ->where('projects_imgs.id_project','=',$project->id)
-            ->get();
-            @endphp
-            @if (count($imagen) == 0)
-                <img src="{{ asset('projects_imgs/sinimagen.png') }}" class="img-obra" width="325" height="310"
-                style="border: 2px solid rgb(180, 180, 180)" alt="">
-            @else
-                <img src="{{ asset('projects_imgs/'.$imagen->last()->imgroute) }}" class="img-obra" width="325" height="310" alt="">
-            @endif
-
-            @php
-                $title = substr($project->title,0,36).'...';
-                $description = substr($project->description,0,140).'...';
-            @endphp
     <div class="container mt-5">
         <div class="row mb-5">
             <div class="col-lg-6">
@@ -677,7 +653,7 @@ Listado de obras
                                             @endphp
                             
                                         <div class="media-body">
-                                            <div class="mt-5 mt-sm-4 ml-5 title-obra">{{ $title }} {{$contar7}} </div>
+                                            <div class="mt-5 mt-sm-4 ml-5 title-obra">{{ $title }}  </div>
                                             <br class="hidden-desktop">
                                             <div class="col-md-10 col-sm-10 col-12 px-0 description-obra">
                                                 <span> {{ $description }} </span>
@@ -778,36 +754,20 @@ Listado de obras
        
         
 
-            <div class="media-body">
-                <div class="mt-5 mt-sm-4 ml-5 title-obra" style=" text-transform: uppercase;">{{ $title }}</div>
-                <br class="hidden-desktop">
-                <div class="col-md-10 col-sm-10 col-12 px-0 description-obra">
-                    <span> {{ $description }} </span> 
-                </div>
-                <div class="form-row">
-                    <div class="mt-4 form-group col-md-3 col-sm-3 col-6 ml-5 container-organismo-publico">
-                        <span class="organismo-publico">Gobierno de Jalisco</span>
+        {{-- <div class="row mb-5">
+            <div class="col-lg-5">
+                <a href="">
+                    <div class="container-item d-flex justify-content-between align-items-center text-white">
+                        <img src="{{ asset('assets/img/project/institucion.png') }}" class="img-fluid" width="36" alt="">
+                        <strong>Ayuntamiento de Zapopan</strong>
+                        <img src="{{ asset('assets/img/project/flecha.png') }}" class="img-fluid icon-arrow" alt="">
                     </div>
-                    <div class="form-group mt-4 col-md-2 col-sm-3 col-6 container-btn">
-                        <a href="{{ route('project-single', $project->id) }}" class="btn btn-sm btn-conoce-mas">
-                            Conoce más
-                        </a>
-                    </div>
-                </div>
-                <div class="col-md-11 d-flex justify-content-end align-items-baseline bg-red">
-                    <span class="porcentaje">{{$project->porcentaje_obra}}%</span>&nbsp;&nbsp;<span
-                        style="color:white">completado</span>
-                </div>
+                </a>
             </div>
-        </div>
-        <br><br>
-        @endforeach
-    </div>
         </div> --}}
     {{-- </div> --}}
 
 </div>
-<br class="hidden-phone"><br class="hidden-phone">
 
 @endsection
 @section('scripts')
