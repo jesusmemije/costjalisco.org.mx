cd6fde04183cc20c87b127c7b180fa7ca8c1425d
Pablo98d <60664025+Pablo98d@users.noreply.github.com>
2021-02-10 14:15:44 -0600

    [UPDATE]
    Actualización guardar imagen

--- app/Http/Controllers/Admin/NewsletterController.php
+++ app/Http/Controllers/Admin/NewsletterController.php
@@ -96,7 +96,10 @@ class NewsletterController extends Controller
 
             $file=$request->file('img_rute_new');
             $name=time().'_'.$file->getClientOriginalName();
-            $file->move(public_path().'..Respaldo/img/',$name);
+
+            //$file->move('costjalisco.org.mx/news_imgs/',$name);
+            $url = $_SERVER['DOCUMENT_ROOT'].'\news_imgs';
+            $file->move($url,$name);
             $ruta='/news_imgs/'.$name;
         
         }
@@ -105,7 +108,6 @@ class NewsletterController extends Controller
         } else {
             $img_rute=$ruta;
         }
-        
         
         Newsletter::where('id','=',$id)->update([
             'title'     => $request->title,
